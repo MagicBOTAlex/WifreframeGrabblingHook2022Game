b6f9bb89888b209b404d1ad84fcfcf4385eef43b
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2022-05-30 22:43:09 +0200

    grav

--- Assets/Scripts/Character/CharacterHandler.cs
+++ Assets/Scripts/Character/CharacterHandler.cs
@@ -2,39 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterHandler : MonoBehaviour
 {
-    private Rigidbody m_PlayerBody;
+    // private Rigidbody m_PlayerBody;
     private Camera m_Camera;
+    private CharacterController m_CharacterController;
 
     [Header("Character controller settings")]
-    [SerializeField] private float m_MovementSpeed;
-    [SerializeField] private float m_SprintSpeed;
-    [SerializeField] private string m_SprintButtonName;
-    [SerializeField] private float m_JumpForce;
+    [SerializeField] private float m_MovementSpeed = 5f;
+    [SerializeField] private float m_MoveSmoothDelta = 0.1f;
+    [SerializeField] private float m_JumpForce = 5f;
+    [SerializeField] private float m_Gravity = 9.82f;
 
     [Header("Camera settings")]
-    [SerializeField] private float m_HorizontalSensitivity;
-    [SerializeField] private float m_VerticalSensitivity;
-    [SerializeField] private float m_MaxViewAngle;
-    [SerializeField] private float m_MinViewAngle;
+    [SerializeField] private float m_HorizontalSensitivity = 400f;
+    [SerializeField] private float m_VerticalSensitivity = 400f;
+    [SerializeField] private float m_MouseSmoothDelta = 0.01f;
+    [SerializeField] private float m_MaxViewAngle = -90f;
+    [SerializeField] private float m_MinViewAngle = 90f;
 
-    private Vector3 m_MovementInput;
-    private Vector2 m_MouseInput;
+    private Vector3 m_MovementVelocity = Vector3.zero;
+    private Vector3 m_MovementVelocityDelta = Vector3.zero;
+    private Vector3 m_CurrentMovementDir = Vector3.zero;
+    private Vector2 m_MouseVelocityDelta = Vector3.zero;
+    private Vector2 m_CurrentMouseDir = Vector3.zero;
     private bool m_IsGrounded;
-    private bool m_IsRunning;
     private bool m_IsWalking;
     private bool m_IsAirborne;
     private bool m_IsIdle;
     private bool m_LockCamera;
     private float m_CameraPitch;
 
-    private void Awake()
-    {
-        m_PlayerBody = GetComponent<Rigidbody>();
-    }
     void Start()
     {
+        m_CharacterController = GetComponent<CharacterController>();
         m_Camera = GameManager.Instance.Camera.GetComponent<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false; 
@@ -42,14 +43,7 @@ public class CharacterController : MonoBehaviour
 
     private void Update() 
     {
-        // Update input vectors
-        m_MovementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        m_MovementInput.Normalize();
-        m_MouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-
         m_IsGrounded = CheckGroundState();
-
-        // Debug.Log(m_IsRunning);
 
         HandleState();
         HandleMovement();
@@ -61,17 +55,31 @@ public class CharacterController : MonoBehaviour
     /// </summary>
     private void HandleMovement() 
     {
-        // Vary speed if running
-        float moveSpeed = m_MovementSpeed;
-        if (m_IsRunning)
-            moveSpeed = m_SprintSpeed;
+        // Get unit vector representing the target direction in which the player should move in it's local axis
+        Vector3 targetDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize();
 
-        Debug.Log(moveSpeed);
-        // Apply movement relative to players local axis
-        Vector3 moveVector = m_MovementInput * moveSpeed * Time.deltaTime;
-        transform.Translate(moveVector);
+        // Gradually smooth the the current movement dir vector to the target dir, specified by 'm_MoveSmoothDelta'
+        m_CurrentMovementDir = Vector3.SmoothDamp(m_CurrentMovementDir, targetDir, ref m_MovementVelocityDelta, m_MoveSmoothDelta);
 
-        HandleJump();
+        // Gravity
+        if (m_IsGrounded)
+        {
+            m_MovementVelocity.y = 0f;
+            // Jump
+            if (Input.GetButton("Jump"))
+            {
+                m_MovementVelocity.y += m_JumpForce;
+            }
+        }
+        m_MovementVelocity.y -= m_Gravity * Time.deltaTime;
+
+        // Add movement scalar to x- and z-axises, and gravity scalar to y-axis and apply movement on the characters local axis
+        Vector3 smoothedMoveVelocity = (transform.forward * m_CurrentMovementDir.z + transform.right * m_CurrentMovementDir.x) * m_MovementSpeed + Vector3.up * m_MovementVelocity.y;
+        m_MovementVelocity.x = smoothedMoveVelocity.x;
+        m_MovementVelocity.z = smoothedMoveVelocity.z;
+        m_CharacterController.Move(smoothedMoveVelocity * Time.deltaTime);
+
     }
 
     /// <summary>
@@ -92,32 +100,24 @@ public class CharacterController : MonoBehaviour
         if (m_LockCamera)
             return;
 
-        // Apply camera's local x rotation (vertical rotation), to camera
-        m_CameraPitch -= m_MouseInput.y * m_VerticalSensitivity * Time.deltaTime;
-        m_CameraPitch = Mathf.Clamp(m_CameraPitch, m_MaxViewAngle, m_MinViewAngle);
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
+        m_CurrentMouseDir = Vector2.SmoothDamp(m_CurrentMouseDir, targetDir, ref m_MouseVelocityDelta, m_MouseSmoothDelta);
+
+        // Apply camera's local x rotation (vertical rotation), to camera
+        m_CameraPitch -= m_CurrentMouseDir.y * m_VerticalSensitivity * Time.deltaTime;
+        m_CameraPitch = Mathf.Clamp(m_CameraPitch, m_MaxViewAngle, m_MinViewAngle);
         m_Camera.transform.localEulerAngles = Vector3.right * m_CameraPitch;
 
         // Apply player model's local y rotation (horizontal rotation), to player model
-        float yawRotation = m_MouseInput.x * m_HorizontalSensitivity * Time.deltaTime;
+        float yawRotation = m_CurrentMouseDir.x * m_HorizontalSensitivity * Time.deltaTime;
         transform.Rotate(Vector3.up * yawRotation);
 
     }
 
-    private void HandleJump()
-    {
-        if (Input.GetButtonDown("Jump") && m_IsGrounded)
-        {
-            m_PlayerBody.AddForce(transform.up * m_JumpForce, ForceMode.Impulse);
-        }
-    }
-
     private bool CheckGroundState()
     {
-        // TODO reimplement this (shoot ray or something)
-        if (m_PlayerBody.velocity.y > 0.01f || m_PlayerBody.velocity.y < -0.01f)
-            return false;
-        return true;
+        return m_CharacterController.isGrounded;
     }
 
     /// <summary>
@@ -126,7 +126,7 @@ public class CharacterController : MonoBehaviour
     private void HandleState()
     {
         // Running
-        if (m_MouseInput.x != 0 || m_MouseInput.y != 0)
+        if (m_CurrentMovementDir.x != 0 || m_CurrentMovementDir.y != 0)
         {
             m_IsIdle = false;
             m_IsWalking = true;
@@ -140,15 +140,6 @@ public class CharacterController : MonoBehaviour
         {
             m_IsAirborne = true;
             m_IsIdle = false;
-        }
-        if (Input.GetButton(m_SprintButtonName))
-        {
-            m_IsRunning = true;
-            m_IsWalking = false;
-        }
-        else
-        {
-            m_IsRunning = false;
         }
     }
 }
