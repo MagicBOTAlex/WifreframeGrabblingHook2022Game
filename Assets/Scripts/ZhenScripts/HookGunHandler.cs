4295826ba1a2bfa037a3f58681d5e421270731b8
BOT Alex <44818698+MagicBOTAlex@users.noreply.github.com>
2022-05-30 14:16:02 +0200

    sdasiudqad

--- Assets/Scripts/ZhenScripts/HookGunHandler.cs
+++ Assets/Scripts/ZhenScripts/HookGunHandler.cs
@@ -34,13 +34,14 @@ public class HookGunHandler : MonoBehaviour
         }
 
         // stores these values to a more readable variable name
+        Quaternion normalRotation = GameManager.Instance.Camera.transform.rotation * StartingRotation;
         Quaternion targetRotation = Quaternion.LookRotation(transform.position - TargetPos);
         Quaternion currentRotation = transform.rotation;
 
         // lerps the rotation of the HookGun to the correct ratation
         transform.rotation = Quaternion.Lerp(
             currentRotation, 
-            (TargetPos == Vector3.zero) ? StartingRotation : targetRotation, // check if there is a target. If target is zero then uses StartingRotation else uses targetRotation
+            (TargetPos == Vector3.zero) ? normalRotation : targetRotation, // check if there is a target. If target is zero then uses StartingRotation else uses targetRotation
             Time.deltaTime * LookingSpeed);
 
     }
