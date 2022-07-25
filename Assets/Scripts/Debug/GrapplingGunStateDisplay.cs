23bf108369dd39f468547e701b3776ca4f067861
sveske_juice <carl.benjamin.dreyer@gmail.com>
2022-07-25 15:19:41 +0200

    Grappling Gun State Debug Display

---
+++ Assets/Scripts/Debug/GrapplingGunStateDisplay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrapplingGunStateDisplay : MonoBehaviour
+{
+    public Text m_StateTextField;
+
+    GrappligGun m_GrapplingGun;
+
+    void Start()
+    {
+        m_GrapplingGun = GameManager.Instance.GrapplingGun.GetComponent<GrappligGun>();
+    }
+    void Update()
+    {
+        string state = m_GrapplingGun.CurrentState.GetType().Name;
+
+        m_StateTextField.text = state;
+    }
+}
